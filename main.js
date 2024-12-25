83dc2a81d2df64dbe4d8634012b774d2a37d3dce
Arinuzal Rama Desta <arnzlrd@gmail.com>
2024-12-26 00:28:51 +0700

    Create add Book

--- main.js
+++ main.js
@@ -1,2 +1,41 @@
 // Do your work here...
-console.log('Hello, world!');
+
+// let bookItem = document.getElementById("bookItem");
+
+bookItem = [];
+console.log(bookItem);
+const generateId = () => {
+  return +new Date();
+};
+
+const generateBookObject = (id, title, author, year, isComplete) => {
+  {
+    id, title, author, year, isComplete;
+  }
+};
+
+const addBook = () => {
+  const bookFormTitle = document.getElementById("bookFormTitle").value;
+  const bookFormAuthor = document.getElementById("bookFormAuthor").value;
+  const bookFormYear = document.getElementById("bookFormYear").value;
+
+  const generateID = generateId();
+  const bookObject = generateBookObject(
+    generateID,
+    bookFormTitle,
+    bookFormAuthor,
+    bookFormYear,
+    false
+  );
+  bookItem.push(bookObject);
+  console.log(bookFormTitle, bookFormAuthor, bookFormYear);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const submitForm = document.getElementById("bookForm");
+
+  submitForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addBook();
+  });
+});
