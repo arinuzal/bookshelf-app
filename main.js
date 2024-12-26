51c37a4ca3d6ffc1d070f7f256f4c3e83b0818ab
Arinuzal Rama Desta <arnzlrd@gmail.com>
2024-12-26 21:44:33 +0700

    Update CreateBook

--- main.js
+++ main.js
@@ -2,17 +2,18 @@
 
 // let bookItem = document.getElementById("bookItem");
 
-bookItem = [];
-console.log(bookItem);
+book = [];
+console.log(book);
+
 const generateId = () => {
   return +new Date();
 };
 
-const generateBookObject = (id, title, author, year, isComplete) => {
-  {
-    id, title, author, year, isComplete;
-  }
-};
+// const generateBookObject = (id, title, author, year, isComplete) => {
+//   return {
+//     id, title, author, year, isComplete
+//   }
+// };
 
 const addBook = () => {
   const bookFormTitle = document.getElementById("bookFormTitle").value;
@@ -20,16 +21,32 @@ const addBook = () => {
   const bookFormYear = document.getElementById("bookFormYear").value;
 
   const generateID = generateId();
-  const bookObject = generateBookObject(
-    generateID,
-    bookFormTitle,
-    bookFormAuthor,
-    bookFormYear,
-    false
-  );
+  // const bookObject = generateBookObject(
+  //   generateID,
+  //   bookFormTitle,
+  //   bookFormAuthor,
+  //   bookFormYear,
+  //   false
+  // );
+  const bookObject = {
+    id: generateID,
+    title: bookFormTitle,
+    author: bookFormAuthor,
+    year: bookFormYear,
+    isComplete: false
+  }
+
   bookItem.push(bookObject);
-  console.log(bookFormTitle, bookFormAuthor, bookFormYear);
+  document.dispatchEvent(new Event(RENDER_EVENT))
+  console.log(bookObject);
 };
+
+const makeBook = (bookObject) => {
+  const {id, title,author, year, isComplete} = bookObject
+  const bookItemTitle = document.getElementById("bookItemTitle")
+  bookItemTitle.innerText = title
+  console.log(title)
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const submitForm = document.getElementById("bookForm");
@@ -39,3 +56,10 @@ document.addEventListener("DOMContentLoaded", () => {
     addBook();
   });
 });
+
+document.addEventListener("RENDER_EVENT", () => {
+   for (bookItem of book) {
+    const bookElement = makeBook(bookItem);
+    console.log(bookItem)
+   }
+})
