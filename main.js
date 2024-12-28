d2945a7ca3e3c6b194627c59e65913cdb3112ffc
Arinuzal Rama Desta <arnzlrd@gmail.com>
2024-12-29 00:01:47 +0700

    Create fitur Complete, Delete and Edit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Do your work here...
 
 book = [];
-const RENDER_EVENT = 'render-todo';
+const RENDER_EVENT = "render-todo";
 console.log(book);
 
 const generateId = () => {
@@ -13,15 +13,15 @@ const addBook = () => {
   const bookFormTitle = document.getElementById("bookFormTitle").value;
   const bookFormAuthor = document.getElementById("bookFormAuthor").value;
   const bookFormYear = document.getElementById("bookFormYear").value;
-  const bookFormIsComplete = document.getElementById('bookFormIsComplete').checked;
+  const bookFormIsComplete = document.getElementById("bookFormIsComplete").checked;
 
   const bookObject = {
     id: generateID,
     title: bookFormTitle,
     author: bookFormAuthor,
     year: bookFormYear,
-    isCompleted: bookFormIsComplete
-  }
+    isCompleted: bookFormIsComplete,
+  };
 
   book.push(bookObject);
   document.dispatchEvent(new Event(RENDER_EVENT));
@@ -30,23 +30,64 @@ const addBook = () => {
 const makeBook = (bookObject) => {
   const { id, title, author, year, isCompleted } = bookObject;
 
-  const textTitle = document.createElement('h3');
+  const textTitle = document.createElement("h3");
+  textTitle.setAttribute("data-testid", "bookItemTitle");
   textTitle.innerText = title;
 
-  const textAuthor = document.createElement('p');
+  const textAuthor = document.createElement("p");
+  textAuthor.setAttribute("data-testid", "bookItemAuthor");
   textAuthor.innerText = author;
 
-  const textYear = document.createElement('p');
+  const textYear = document.createElement("p");
+  textYear.setAttribute("data-testid", "bookItemYear");
   textYear.innerText = year;
 
-  // Bungkus elemen teks ke dalam kontainer
-  const textContainer = document.createElement('div');
-  textContainer.setAttribute('data-bookid', id);
-  textContainer.setAttribute('data-testid', 'bookItem');
-  textContainer.append(textTitle, textAuthor, textYear);
+  const isCompleteButton = document.createElement("button");
+  isCompleteButton.setAttribute("data-testid", "bookItemIsCompleteButton");
+  isCompleteButton.innerText = "Selesai dibaca";
 
-  return textContainer;
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("data-testid", "bookIteDeleteButton");
+  deleteButton.innerText = "Hapus buku";
+
+  const editButton = document.createElement("button");
+  editButton.setAttribute("data-testid", "bookItemEditButton");
+  editButton.innerText = "Edit buku";
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.append(isCompleteButton, deleteButton, editButton);
+
+  const container = document.createElement("div");
+  container.setAttribute("data-bookid", id);
+  container.setAttribute("data-testid", "bookItem");
+  container.append(textTitle, textAuthor, textYear, buttonContainer);
+
+  console.log(container);
+  
+  if(isCompleted === false) {
+      deleteButton.addEventListener('click', (event) => {
+      event.preventDefault()
+      isCompleteBook(isCompleted)
+    })
+  } else {
+     console.log("False")
+  }
+
+  return container;
 };
+
+ const isCompleteBook = (isCompleted) => {
+   isCompleted = true
+   document.dispatchEvent(new Event(RENDER_EVENT))
+ }
+
+ const deleteBook = () => {
+
+ }
+
+ const editBook = () => {
+
+ }
 
 // const findBook = (bookId) => {
 //   return book.find((bookItem) => bookItem.id === bookId) || null;
@@ -54,7 +95,7 @@ const makeBook = (bookObject) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const submitForm = document.getElementById("bookForm");
-  
+
   submitForm.addEventListener("submit", (event) => {
     event.preventDefault();
     addBook();
@@ -62,18 +103,18 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener(RENDER_EVENT, () => {
-  const incompleteBookList = document.getElementById('incompleteBookList')
-  const completeBookList = document.getElementById('completeBookList')
-  
-  incompleteBookList.innerHTML = '';
-  completeBookList.innerHTML = '';
+  const incompleteBookList = document.getElementById("incompleteBookList");
+  const completeBookList = document.getElementById("completeBookList");
 
-   for (bookItem of book) {
+  incompleteBookList.innerHTML = "";
+  completeBookList.innerHTML = "";
+
+  for (bookItem of book) {
     const bookElement = makeBook(bookItem);
     if (bookItem.isCompleted) {
-      completeBookList.append(bookElement)
+      completeBookList.append(bookElement);
     } else {
-      incompleteBookList.append(bookElement)
-      }
+      incompleteBookList.append(bookElement);
+    }
   }
-})
+});
